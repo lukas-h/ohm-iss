faf48ab2da641cca023c8c0c8548a18a0baf8aa1
urbanna-hub <anna.graetzer@student.uibk.ac.at>
2024-07-17 09:08:37 +0000

    add exercises

---
+++ 17.js
@@ -0,0 +1,34 @@
+function addAplusBplusC (a,b,c) {
+    return a + b + c
+}
+
+var result = addAplusBplusC ("ohm", "iss", "nbg")
+
+console.log (result
+)
+const names = [
+    Rachele, Anna, Nick, DD, Renata, Maria
+]
+function test (a,b,c){
+    return a+b+c
+
+//for loop
+for
+
+var totalYears = 0 
+var ages = [31, 23, 23, 24, 25, 21]
+
+
+//for loop
+for (age of ages) {
+    totalYears = totalYears + age
+}
+const average = totalYears / ages.length
+console.log ("total: " + totalYears)
+console.log ("average: " + average)
+
+//1st iteration: totalYears = 0 + 31
+//2nd iteration: totalYears = 31 + 23
+//3rd iteration: totalYears = 54 + 23
+//4th iteration: totalYears = 77 + 24
+//
