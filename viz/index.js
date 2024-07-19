f65727efec2241561e48d50fc29fba922f168cb0
Lukas Himsel <lukas@himsel.me>
2024-07-19 09:35:00 +0000

    change color

--- viz/index.js
+++ viz/index.js
@@ -1,4 +1,5 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map');
+map.setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -6,5 +7,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var geoJson = { "type": "FeatureCollection", "features": [{ "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[[9.2202794, 49.1457732], [9.2203015, 49.1458705], [9.2205046, 49.1458507], [9.2204825, 49.1457535], [9.2202794, 49.1457732]]] } }] };
 
-var resultLayer = L.geoJson(geoJson).addTo(map);
-map.fitBounds(inputLayer.getBounds());
+var resultLayer = L.geoJson(geoJson, {
+    fillColor: 'red',
+    color: 'red',
+});
+resultLayer.addTo(map);
+
+map.fitBounds(resultLayer.getBounds());
